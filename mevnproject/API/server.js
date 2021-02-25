6edf6ba6a5c13a01da8c6b24c1011684287042ab
bULLETHELL <polyswing7@hotmail.com>
2021-02-25 14:54:37 +0100

    AdminBro

--- mevnproject/API/server.js
+++ mevnproject/API/server.js
@@ -8,19 +8,34 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./DB.js');
 const postRoute = require('./post.route');
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('@admin-bro/express')
+const AdminBroMongoose = require('@admin-bro/mongoose')
+
+require('./post.model')
+
+AdminBro.registerAdapter(AdminBroMongoose)
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-  () => { console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)}
+const connection = mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    () => { console.log('Database is connected') },
+    err => { console.log('Can not connect to the database' + err) }
 );
 
+const adminBro = new AdminBro({
+    Databases: [connection],
+    rootPath: '/admin',
+})
+
+const router = AdminBroExpress.buildRouter(adminBro)
+
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/posts', postRoute);
+app.use(adminBro.options.rootPath, router)
 
-app.listen(PORT, function(){
-  console.log('Server is running on Port:',PORT);
+app.listen(PORT, function() {
+    console.log('Server is running on Port:', PORT);
 });
