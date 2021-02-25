4a351139f8c06e0c71d4e1919b5d63c0a42a7eac
FunkyXive <andreasnordenhof@gmail.com>
2021-02-25 10:04:20 +0100

    init

---
+++ mevnproject/src/App.vue
@@ -0,0 +1,32 @@
+// App.vue
+
+<template>
+ <div >
+  <nav >
+   <ul >
+    <li >
+     <router-link to="/" >Home</router-link>
+    </li>
+    <li >
+     <router-link to="/create" >Create Post</router-link>
+    </li>
+    <li >
+     <router-link to="/posts" >Posts</router-link>
+    </li>
+   </ul>
+  </nav><br />
+  <transition name=fade>
+   <router-view></router-view>
+  </transition>
+ </div>
+</template>
+
+<style>
+  
+</style>
+
+<script>
+
+  export default{
+  }
+</script>
---
+++ mevnproject/src/components/EditComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <div>
+                <div>Edit Component</div>
+
+                <div>
+                    I'm an Edit component.
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
