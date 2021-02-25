04c435b5b842012cdbff23e533168cd540a8a041
bULLETHELL <polyswing7@hotmail.com>
2021-02-25 15:28:49 +0100

    User Scaffolding

---
+++ mevnproject/API/models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Define collection and schema for Post
+let User = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    encryptedPassword: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'restricted'],
+        required: true
+    },
+}, {
+    collection: "users",
+});
+
+module.exports = mongoose.model("User", User);
